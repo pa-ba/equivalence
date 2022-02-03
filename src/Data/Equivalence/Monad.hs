988b9d67053e990bd069c213d2b3786bf461ce39
Andreas Abel <andreas.abel@ifi.lmu.de>
2022-02-03 13:02:43 +0100

    Derive some instances for EquivT using GeneralizedNewtypeDeriving
    Gets rid of some boilerplate.

--- src/Data/Equivalence/Monad.hs
+++ src/Data/Equivalence/Monad.hs
@@ -1,10 +1,10 @@
-{-# LANGUAGE
-  RankNTypes,
-  FlexibleInstances,
-  FlexibleContexts,
-  MultiParamTypeClasses,
-  UndecidableInstances,
-  FunctionalDependencies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 --------------------------------------------------------------------------------
 -- |
@@ -58,7 +58,7 @@ descriptors of type @c@ and contains an internal monadic computation
 of type @m a@. -}
 
 newtype EquivT s c v m a = EquivT {unEquivT :: ReaderT (Equiv s c v) (STT s m) a}
-
+  deriving (Functor, Applicative, Monad, MonadError e, MonadState st, MonadWriter w)
 
 {-| This monad transformer is a special case of 'EquivT' that only
 maintains trivial equivalence class descriptors of type @()@. -}
@@ -73,22 +73,12 @@ over elements of type @v@ with equivalence class descriptors of type
 
 type EquivM s c v = EquivT s c v Identity
 
-
 {-| This monad is a special case of 'EquivM' that only maintains
 trivial equivalence class descriptors of type @()@. -}
 
 type EquivM' s v = EquivM s () v
 
-instance Functor m => Functor (EquivT s c v m) where
-  fmap f (EquivT m) = EquivT $ fmap f m
-
-instance (Applicative m, Monad m) => Applicative (EquivT s c v m) where
-  pure = EquivT . pure
-  (EquivT f) <*> (EquivT a) = EquivT (f <*> a)
-
-instance (Monad m) => Monad (EquivT s c v m) where
-    EquivT m >>= f = EquivT (m >>= (unEquivT . f))
-    return = pure
+-- Instances for EquivT:
 
 instance MonadTrans (EquivT s c v) where
     lift = EquivT . lift . lift
@@ -96,22 +86,11 @@ instance MonadTrans (EquivT s c v) where
 instance Monad m => Fail.MonadFail (EquivT s c v m) where
     fail = error
 
+-- NB: This instance is beyond GeneralizedNewtypeDeriving
+-- because EquivT already contains a ReaderT in its monad transformer stack.
 instance (MonadReader r m) => MonadReader r (EquivT s c v m) where
     ask = EquivT $ lift ask
     local f (EquivT (ReaderT m)) = EquivT $ ReaderT $ (\ r -> local f (m r))
-
-instance (Monoid w, MonadWriter w m) => MonadWriter w (EquivT s c v m) where
-    tell w = EquivT $ tell w
-    listen (EquivT m) = EquivT $ listen m
-    pass (EquivT m) = EquivT $ pass m
-
-instance (MonadState st m) => MonadState st (EquivT s c v m) where
-    get = EquivT get
-    put s = EquivT $ put s
-
-instance (MonadError e m) => MonadError e (EquivT s c v m) where
-    throwError e = lift $ throwError e
-    catchError (EquivT m) f = EquivT $ catchError m (unEquivT . f)
 
 {-| This function runs a monadic computation that maintains an
 equivalence relation. The first two arguments specify how to construct
